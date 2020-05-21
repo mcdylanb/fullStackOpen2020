59d7fe682a63d9b36f49920d7528250f02488a9a
mcdylanb <dylansatgalab@gmail.com>
2020-05-21 15:44:26 +0800

    Initial required for the exercise, error prone

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+    return(
+        <div>
+            <h1>{props.course.name}</h1>
+        </div>
+    )
+}
+
+const Content = (props) => {
+    return(
+        <div>
+            <p>{props.parts[0].name} : {props.parts[0].exercises}</p>
+            <p>{props.parts[1].name} : {props.parts[1].exercises}</p>
+            <p>{props.parts[2].name} : {props.parts[2].exercises}</p>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return(
+        <div>
+            <p> Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+        </div>
+    )
+}
+const App = () => {
+
+    const course = {
+        id: 1,
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises : 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises : 14,
+                id: 3
+            }
+        ]
+    }
+    return <Course course={course}/>
+}
+ReactDOM.render(<App />, document.getElementById('root'))
