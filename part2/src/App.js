63261e364bd545b77edd8ac1ab93f0b7aca183fc
mcdylanb <dylansatgalab@gmail.com>
2020-05-22 13:56:07 +0800

    added button to showimportant or show all

--- part2/src/App.js
+++ part2/src/App.js
@@ -25,12 +25,17 @@ const App = (props) => {
   }
 
   const notesToShow = showAll
-  ? notes
-        : notes.filter(note => note.important === true)
+        ? notes
+        : notes.filter(note => note.important )
 
     return (
         <div>
           <h1>Notes</h1>
+          <div>
+            <button onClick={() => setShowAll(!showAll)}>
+              show {showAll ? 'important' : 'all'}
+            </button>
+          </div>
           <ul>
             {notesToShow.map(note =>
                        <Note key={note.id} note={note}/>
